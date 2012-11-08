c4aa7e61955c304523558e59fc6f92b5aac31c13
unknown <christopher.chau@gmail.com>
2012-11-08 17:51:01 +0900

    added solution to problem 003, it is slow though

---
+++ 003/ProjectEulerProblem003.rb
@@ -0,0 +1,40 @@
+startTime = Time.now
+number = 500#600851475143
+squareRootedNumber = Math.sqrt(number)
+result = 0
+
+sift = Array.new(squareRootedNumber)
+existingPrimeNumbers = Array.new()
+
+def IsPrime(number, existingPrimeNumbers)
+  existingPrimeNumbers.each do |primeNumber|
+    if (number % primeNumber == 0)
+      return false
+    end   
+  end
+  
+  return true
+end
+
+
+for counter in 2..squareRootedNumber
+  if (sift[counter].nil?)
+    if (IsPrime(counter, existingPrimeNumbers))
+      existingPrimeNumbers.push(counter)
+      
+      for siftIndex in counter..squareRootedNumber
+        if (siftIndex % counter == 0)
+          sift[siftIndex] = "Not Prime"                   
+        end
+      end
+      
+      if (number % counter == 0)
+        puts (counter).to_s + " and " +  (number / counter).to_s
+        result = counter
+      end
+    end
+  end
+end
+
+puts "The largest divisible prime: " + result.to_s
+puts "Time took: " + (Time.now - startTime).to_s + " seconds"
