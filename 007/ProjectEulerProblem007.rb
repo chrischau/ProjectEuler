2dfc551a2e9d46dc0fe40f5a00f5ac1ebc291e3f
chrischau <christopher.chau@gmail.com>
2013-11-28 12:57:49 -0500

    improved performance using ruby's library, cheating?

--- 007/ProjectEulerProblem007.rb
+++ 007/ProjectEulerProblem007.rb
@@ -1,38 +1,13 @@
+require 'mathn'
+
 startTime = Time.now
-number = 1000000
 
-result = 0
-
-sift = Array.new(number)
-existingPrimeNumbers = Array.new()
-
-def IsPrime(number, existingPrimeNumbers)
-  existingPrimeNumbers.each do |primeNumber|
-    if (number % primeNumber == 0)
-      return false
-    end   
-  end
-  
-  return true
-end
-
-
-for counter in 2..number
-  if (sift[counter].nil?)
-    if (IsPrime(counter, existingPrimeNumbers))
-      existingPrimeNumbers.push(counter)
-      puts existingPrimeNumbers.length.to_s + " : " + counter.to_s
-      if (existingPrimeNumbers.length == 10001)
-        puts "1001st prime: " + counter.to_s
-        break
-      end
-      
-      for siftIndex in counter..number
-        if (sift[siftIndex].nil? and (siftIndex % counter == 0))
-          sift[siftIndex] = "Not Prime"                   
-        end
-      end      
-    end
+counter = 1
+for p in Prime.instance
+  counter += 1
+  if (counter == 10001)
+    puts "1001st prime: " + p.to_s
+    break
   end
 end
 
