08b67e1a7a5894c039a299d1befba8909ee0c383
Chris Chau <christopher.chau@gmail.com>
2023-03-06 22:15:09 +0900

    implemented and solved the problem

--- 019/ProjectEulerProblem019/Program.cs
+++ 019/ProjectEulerProblem019/Program.cs
@@ -1,7 +1,20 @@
 ﻿namespace ProjectEulerProblem019 {
 	internal class Program {
 		static void Main(string[] args) {
-			Console.WriteLine("Hello, World!");
+			int sundayCount = 0;
+
+			var day = new DateTime(1901, 1, 1);
+			var lastDay = new DateTime(2000, 12, 31);
+
+			while (day <= lastDay) {
+				if (day.DayOfWeek == DayOfWeek.Sunday	&& day.Day == 1) {
+					sundayCount++;
+					//Console.WriteLine(day);
+				}
+				day = day.AddDays(1);
+			}
+
+			Console.WriteLine("Total number of Sundays: {0}", sundayCount); //171
 		}
 	}
 }
