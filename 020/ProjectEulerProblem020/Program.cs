e9b3c108417581e66fd4ad03ce8cb0ac0f88a86b
Chris Chau <christopher.chau@gmail.com>
2023-03-07 21:50:58 +0900

    implemented and solved problem 20

--- 020/ProjectEulerProblem020/Program.cs
+++ 020/ProjectEulerProblem020/Program.cs
@@ -1,7 +1,24 @@
-﻿namespace ProjectEulerProblem020 {
+﻿using System.Numerics;
+
+namespace ProjectEulerProblem020 {
 	internal class Program {
 		static void Main(string[] args) {
-			Console.WriteLine("Hello, World!");
+			int sum = 0;
+			BigInteger factorialSum = 1;
+			
+			for (int i = 2; i <= 99; i++) {
+				factorialSum *= i;
+			}
+
+			Console.WriteLine("100 Factorial Sum: {0}", factorialSum);
+
+			foreach (var singleDigit in factorialSum.ToString().ToCharArray()) {
+				int tempNumber = Convert.ToInt32(singleDigit.ToString());
+				sum += tempNumber;
+				//Console.WriteLine("{0} - {1}", tempNumber, sum);
+			}
+			
+			Console.WriteLine("Sum of 100 Factorial: {0}", sum); //648
 		}
 	}
 }
