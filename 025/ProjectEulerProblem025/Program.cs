fd5c47bdfd162f96414ffad993bd925ce5a91543
Chris Chau <christopher.chau@gmail.com>
2023-03-13 03:55:24 +0900

    changed the return type

--- 025/ProjectEulerProblem025/Program.cs
+++ 025/ProjectEulerProblem025/Program.cs
@@ -3,13 +3,17 @@
 namespace ProjectEulerProblem025 {
 	internal class Program {
 		static void Main(string[] args) {
+			bool found = false;
+
 			for (BigInteger i = 0; i <= 4785; i++) {
-				fibonacci(i);
+				if (!found) {
+					found = fibonacci(i);
+				}
 			}
-			
+
 		}
 
-		static void fibonacci(BigInteger len) {
+		static bool fibonacci(BigInteger len) {
 			BigInteger a = 0, b = 1, c = 0;
 
 			//Console.Write("{0} {1}", a, b);
@@ -17,14 +21,16 @@ namespace ProjectEulerProblem025 {
 			for (BigInteger i = 2; i < len; i++) {
 				c = a + b;
 				//Console.Write(" {0}", c);
-				
-				if (c.ToString().Length == 1000) {
-					Console.WriteLine("{0} - {1}", len-1, c);
-					break;
-				}
 				a = b;
 				b = c;
+
+				if (c.ToString().Length == 1000) {
+					Console.WriteLine("{0} - {1}", len - 1, c);
+					return true;
+				}
 			}
+
+			return false;
 		}
 	}
 }
