02b09d53f6bf9a96f9b412a1b9ea81b246d019a3
Chris Chau <christopher.chau@gmail.com>
2023-03-13 01:43:54 +0900

    initial check in, I dont understand this problem. small abundant number is 12, small sum of 2 abundant number is 24. 24 - 12 = 12.  I dont understand

---
+++ 023/ProjectEulerProblem023/Program.cs
@@ -0,0 +1,43 @@
+﻿namespace ProjectEulerProblem023 {
+	internal class Program {
+
+		const string Abundant = "abundant";
+		const string Perfect = "perfect";
+		const string Deficient = "deficient";
+
+		static void Main(string[] args) {
+			var numberDictionary = new Dictionary<int, string>();
+
+			for (int i = 12; i < 28123; i++) {
+				numberDictionary.Add(i, Dividor(i));
+			}
+
+			foreach (int i in numberDictionary.Keys) {
+				if (numberDictionary[i] == Abundant) {
+					Console.WriteLine(i);
+				}
+			}
+		}
+
+
+		static string Dividor(int number) {
+			var workingNumbers = new List<int>();
+
+			for (int i = 1; i < number; i++) {
+				if (number % i == 0) {
+					workingNumbers.Add(i);
+				}
+			}
+
+			var sum = workingNumbers.Sum();
+			if (sum > number) {
+				return Abundant;
+			}
+			else if (sum == number) {
+				return Perfect;
+			}
+			return Deficient;
+		}
+
+	}
+}
