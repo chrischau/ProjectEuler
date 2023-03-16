8b073dcbf31edd1b2f7f8825d3b42f6c0c4e16df
Chris Chau <christopher.chau@gmail.com>
2023-03-17 00:23:40 +0900

    used factorial division to find solution

--- 024/ProjectEulerProblem024/Program.cs
+++ 024/ProjectEulerProblem024/Program.cs
@@ -1,22 +1,27 @@
-﻿namespace ProjectEulerProblem024 {
+﻿using static System.Runtime.InteropServices.JavaScript.JSType;
+using System;
+
+namespace ProjectEulerProblem024 {
 	internal class Program {
 		static int count = 0;
 
 		static void Main(string[] args) {
-			string str = "0123456789";
-			char[] charArry = str.ToCharArray();
-			Permute(charArry, 0, str.Length-1);
-			Console.WriteLine("Number of permutation: {0}", count);
+			//string str = "0123456789";
+			//char[] charArry = str.ToCharArray();
+			//Permute(charArry, 0, str.Length - 1);
+			//Console.WriteLine("Number of permutation: {0}", count);
+
+			Console.WriteLine(solution(1000000));
 		}
 
 		static void Permute(char[] numbers, int i, int numberToPermut) {
 			int j;
 			if (i == numberToPermut) {
 				count++;
-				Console.WriteLine(numbers);				
+				//Console.WriteLine(numbers);				
 				if (count == 1000000) {
 					//Console.WriteLine("Count {0}", count);
-					//Console.WriteLine(numbers);					
+					Console.WriteLine(numbers);
 				}
 			}
 			else {
@@ -35,6 +40,23 @@
 			tmp = a;
 			a = b;
 			b = tmp;
+		}
+
+		static string solution(int num) {
+			var fac = new List<int> { 1, 1, 2, 6, 24, 120, 720, 5040, 40320, 362880 };
+			var arr = new List<int> { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+			var ret = "";
+
+			num--;
+			for (var i = arr.Count - 1; i >= 0; i--) {
+				var t = num / fac[i] | 0;
+				//Console.WriteLine("{0} - {1} / {2} = {3}, {4}, {5}", i, num, fac[i], t, arr[t], ret); 
+				
+				num %= fac[i];
+				ret += arr[t];
+				arr.RemoveAt(t);
+			}
+			return ret;
 		}
 	}
 }
